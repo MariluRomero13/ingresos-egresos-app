453fc536512c3aacc61b4a1b7f6b412c1cf06aa8
MariluRomero13 <romeromarilu13@gmail.com>
2020-05-24 15:14:28 -0500

    [ADD] components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngredoEgresoComponent } from './ingredo-egreso/ingredo-egreso.component';
+import { EstadisticaComponent } from './ingredo-egreso/estadistica/estadistica.component';
+import { DetalleComponent } from './ingredo-egreso/detalle/detalle.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngredoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ingredo-egreso/ingredo-egreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredo-egreso',
+  templateUrl: './ingredo-egreso.component.html',
+  styles: []
+})
+export class IngredoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
